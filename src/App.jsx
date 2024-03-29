614ea39542a7a11a04a5a06f8ac4127bcd224948
M-HAJITH <121685911+MohamedHajith@users.noreply.github.com>
2024-03-29 20:49:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./App.css"; // Import CSS file
 
-const key = '222f31f47b78be41bec75d130563080c';
+const apikey = import.meta.env.VITE_API_KEY;
 
 const App = () => {
   const [city, setCity] = useState("");
